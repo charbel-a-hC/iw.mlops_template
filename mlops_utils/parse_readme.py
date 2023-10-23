4ebff41383d1035a60f736679aa54a03bb5cc842
charbel-a-hC <charbel-a-h@outlook.com>
2023-10-23 13:16:04 +0300

    re-factored function to handle no URL at initial run

--- mlops_utils/parse_readme.py
+++ mlops_utils/parse_readme.py
@@ -12,19 +12,24 @@ def parse_readme(readme_path):
     return entity_name, project_name, run_id
 
 
-def update_readme(run_id, readme_path):
+def update_readme(run_id, entity, project, readme_path):
+    WANDB_URL = "https://wandb.ai/"
+
     with open(readme_path, "r") as f:
         content = f.readlines()
 
     new_content = []
-    i = 0
-    while i < len(content):
-        if "wandb.ai" in content[i]:
-            new_url = content[i].split("/")[:-1]
-            new_url.append(str(run_id))
-            content[i] = "/".join(new_url)
-        new_content.append(content[i])
-        i += 1
+    
+    new_url = f"{WANDB_URL}{entity}/{project}/runs/{run_id}\n"
+    for line in content:
+        if "wandb.ai" in line:
+            new_content.append(new_url)
+        else:
+            new_content.append(line)
 
     with open(readme_path, "w") as f:
         f.writelines(new_content)
+
+if __name__ == "__main__":
+    # Function testing
+    update_readme(800, entity="idealworks-ml", project="sample-mlops", readme_path="../README.md")
