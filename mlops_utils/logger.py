46e4c5970416a868ce6dfc4246f3ab39ec25d728
charbel-a-hC <charbel-a-h@outlook.com>
2023-10-26 10:44:32 +0200

    adjusted formatting and disabled file creation on logger instatiation

--- mlops_utils/logger.py
+++ mlops_utils/logger.py
@@ -34,7 +34,7 @@ def get_logger(description="project"):
     logger.setLevel(logging.DEBUG)
 
     # Define format for logs
-    fmt = '%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s'
+    fmt = '%(asctime)s.%(msecs)03d [%(levelname)s]: %(message)s'
 
     # Create stdout handler for logging to the console (logs all five levels)
     stdout_handler = logging.StreamHandler()
@@ -43,7 +43,7 @@ def get_logger(description="project"):
 
     # Create file handler for logging to a file (logs all five levels)
     today = datetime.date.today()
-    file_handler = logging.FileHandler(f"{os.getcwd()}/{description}_{today.strftime('%Y_%m_%d')}.log")
+    file_handler = logging.FileHandler(f"{os.getcwd()}/{description}_{today.strftime('%Y_%m_%d')}.log", delay=True)
     file_handler.setLevel(logging.WARNING)
     file_handler.setFormatter(logging.Formatter(fmt))
 
@@ -59,6 +59,3 @@ if __name__ == "__main__":
     logger = get_logger()
     logger.debug('This is a debug-level message')
     logger.info('This is an info-level message')
-    logger.warning('This is a warning-level message')
-    logger.error('This is an error-level message')
-    logger.critical('This is a critical-level message')
